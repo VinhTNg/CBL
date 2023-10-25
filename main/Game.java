2bac0d91956c78989f268a98d85eeeae9b13d9fc
Nam Mai <maihoangnam.study@gmail.com>
2023-10-25 17:49:01 +0200

    almost final

--- main/Game.java
+++ main/Game.java
@@ -135,8 +135,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
                 // bridge.baseY = 500;
 
                 //set Hero back to starting position
-                hero.baseX = 48;
-                hero.baseY = 430;
+                hero.baseX = 18;
+                hero.baseY = 400;
                 hero.move();
                 timerStop = false;
 
@@ -145,7 +145,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
                 newSpace = (int) (randomX.nextFloat() * (maxSpace - minSpace + 1) + minSpace);
                 newX2 = newLimLand1 + newSpace;
                 maxLim2 = width - newX2 - 20;
-                minLim2 = width - newX2 - 50;
+                minLim2 = width - newX2 - 30;
                 newLimLand2 = (int) (randomLimX.nextFloat() * (maxLim2 - minLim2 + 1) + minLim2);
                 firstLand.setBounds(0, 500, newLimLand1, 300);
                 secLand.setBounds(newX2, 500, newLimLand2, 300);
@@ -194,7 +194,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (timer.isRunning()) {
             if (bridge.xRotated >= bridge.length || bridge.yRotated <= 0) {
-                if (hero.baseX < bridge.baseX + bridge.length) {
+                if (hero.baseX < bridge.baseX + bridge.length - 25) {
                     hero.move();
                 } else if (heroLocation < newX2 || heroLocation > newX2 + newLimLand2) {
                     drop = true;
