c4ee2fbbccf1ec8be6669d67c49baafa1c8fabd2
Vinh Nguyen <vinhngth0705@gmail.com>
2023-10-25 16:39:25 +0200

    update game

--- main/Game.java
+++ main/Game.java
@@ -131,7 +131,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
                 drop = false;
                 bridge.length = 0;
                 bridge.xRotated = 0;
-                bridge.baseY = 500;
+                // bridge.baseY = 500;
 
                 //set Hero back to starting position
                 hero.baseX = 48;
@@ -154,7 +154,6 @@ public class Game extends JPanel implements KeyListener, ActionListener {
             }
         }
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            System.out.println("1");
             if (playing) {
                 if (bridge.length < width - hero.baseX) {
                     repaint();
@@ -208,7 +207,6 @@ public class Game extends JPanel implements KeyListener, ActionListener {
                         timerStop = true;
                         startAnimation = false;
                         bridge.length = 0;
-                        bridge.baseY = 500;
                     }
                 } else {
                     timer.stop();
